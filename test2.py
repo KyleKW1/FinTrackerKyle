38631114c06235934f378667c04b377b500b21ea
KyleKW1 <113841695+KyleKW1@users.noreply.github.com>
2025-11-21 14:12:33 -0500

    Refactor default_mapping structure in test2.py

--- test2.py
+++ test2.py
@@ -1012,17 +1012,17 @@ def main_app():
         # Load user preferences
         user_prefs = get_user_preferences(st.session_state.user['id'])
         
-    default_mapping = {
-            "Food": ["juici", "kfc", "restaurant", "burger", "pizza","subway", "mcdonald", "starbucks", "diner", "grill", "v.o.d.a. foods", "cafe blue", "tutti frutti", "popeyesohr","ribbiz lounge", "beifang kitchen"],
-            "Grocery": ["hi-lo", "supermarket", "wholesale","grocery", "market", "walmart", "costco","shoppers fair"],
-            "Utilities": ["jps", "nwc", "flow", "internet", "light", "water","electric", "cable", "wifi","BPYMT"],
-            "Transport": ["uber", "taxi", "gas","shell", "parking", "lyft", "bus"],
-            "Income": ["remitly", "deposit", "transfer", "payroll","salary", "refund", "ELink TRF-FR"],
-            "Home Improvement": ["lumber depot limited", "ping's fabric"],
-            "Retail & Entertainment": ["boss destinations", "n k wholesale liquor stor","digicel ding"],
-            "Miscellaneous": ["atm", "fee", "charge","GCT"],
-            "Other": ["ELink TRF-To"]
-        }
+        default_mapping = {
+                "Food": ["juici", "kfc", "restaurant", "burger", "pizza","subway", "mcdonald", "starbucks", "diner", "grill", "v.o.d.a. foods", "cafe blue", "tutti frutti", "popeyesohr","ribbiz lounge", "beifang kitchen"],
+                "Grocery": ["hi-lo", "supermarket", "wholesale","grocery", "market", "walmart", "costco","shoppers fair"],
+                "Utilities": ["jps", "nwc", "flow", "internet", "light", "water","electric", "cable", "wifi","BPYMT"],
+                "Transport": ["uber", "taxi", "gas","shell", "parking", "lyft", "bus"],
+                "Income": ["remitly", "deposit", "transfer", "payroll","salary", "refund", "ELink TRF-FR"],
+                "Home Improvement": ["lumber depot limited", "ping's fabric"],
+                "Retail & Entertainment": ["boss destinations", "n k wholesale liquor stor","digicel ding"],
+                "Miscellaneous": ["atm", "fee", "charge","GCT"],
+                "Other": ["ELink TRF-To"]
+            }
 
         
         default_budgets = {
