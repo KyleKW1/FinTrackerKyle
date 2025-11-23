fc38f8d7ed9cf3d8c3f6dccc8b37d83c59f53f35
KyleKW1 <113841695+KyleKW1@users.noreply.github.com>
2025-11-22 22:02:43 -0500

    Update test.py

--- test.py
+++ test.py
@@ -20,7 +20,7 @@ import pickle
 from typing import Optional, List  # ← ADD THIS LINE IF MISSING
 import socket
 #from reportlab.lib.pagesizes import letter, A4
-from reportlab.lib import colors
+#from reportlab.lib import colors
 from reportlab.lib.units import inch
 from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph, Spacer, Image, PageBreak
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
