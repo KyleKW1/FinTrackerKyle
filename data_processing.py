e2457bef5b31476b38261d868c1659b0ce595b5c
KyleKW1 <113841695+KyleKW1@users.noreply.github.com>
2025-12-06 02:45:27 -0500

    Update data_processing.py

--- data_processing.py
+++ data_processing.py
@@ -11,6 +11,7 @@ from config import DEFAULT_CATEGORY_MAPPING
 from database import get_user_preferences
 import streamlit as st
 import json
+from io import BytesIO
 
 
 def process_csv(file_bytes):
