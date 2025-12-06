f3f8db20937c8d19d7f70fb0bfd2d711cc728c5c
KyleKW1 <113841695+KyleKW1@users.noreply.github.com>
2025-12-06 05:01:00 -0500

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,7 +1,1 @@
-# config.py
-# App email configuration for sending alerts
-APP_EMAIL = "fintrackeralerts@gmail.com"
-APP_EMAIL_PASSWORD = "myhdkbyrzmpvwjyb"  # Use Gmail App Password
-SMTP_SERVER = "smtp.gmail.com"
-SMTP_PORT = 465
-    # Insert your network analysis visualizations or placeholder text here.
+\
